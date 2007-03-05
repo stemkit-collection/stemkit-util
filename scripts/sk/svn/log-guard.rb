37e5bca027a81fd1220cd5a2e81c82161aa78e61
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2007-03-05 18:33:12 +0000

    Introduced a hook to check an SVN log message for not being empty. It looks for block 'log-guard' in the config file. Parameter 'enforce' turns the check on (true) or off (false). Parameter 'message' specifies a message to be displayed when check fails.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@250 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ scripts/sk/svn/log-guard.rb
@@ -0,0 +1,46 @@
+=begin
+  Copyright (c) 2007, Gennady Bystritsky <bystr@mac.com>
+  
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+=end
+
+require 'sk/svn/hook/plugin/generic.rb'
+require 'tsc/border-box.rb'
+
+module SK
+  module Svn
+    class LogGuard < Hook::Plugin::Generic
+      def process(info)
+        guard = config.repositories[info.depot]['log-guard'] or return 
+
+        return unless guard['enforce'] == true
+        return unless info.log.to_s.strip.empty?
+
+        message = guard['message']
+        $stderr.puts TSC::BorderBox[ message ] if message
+
+        raise 'Empty log message'
+      end
+    end
+  end
+end
+
+if $0 == __FILE__ or defined?(Test::Unit::TestCase)
+  require 'test/unit'
+  require 'mocha'
+  require 'stubba'
+  
+  module SK
+    module Svn
+      class AccessModeratorTest < Test::Unit::TestCase
+        def setup
+        end
+        
+        def teardown
+        end
+      end
+    end
+  end
+end
