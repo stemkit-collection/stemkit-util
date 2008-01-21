69fa5fec36d00a49182d01dbbf3200a063b0647d
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-01-21 18:52:04 +0000

    Fixed a script created on Windows to work on Unix.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@474 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/ensure-unix-eol.rb
+++ scripts/ensure-unix-eol.rb
@@ -1,10 +1,13 @@
+#!/usr/bin/env ruby
 
 ARGV.each do |_file|
   content = File.open(_file, "r") do |_input|
-    _input.readlines
+    _input.readlines.map { |_line|
+      _line.chomp
+    }
   end
 
   File.open(_file, "wb") do |_output|
-    _output.write(content)
+    _output.puts(content)
   end
 end
