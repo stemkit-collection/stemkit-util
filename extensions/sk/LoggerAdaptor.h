606ea704e76d691db5d859fd1e05018563601a32
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-02-03 08:35:05 +0000

    Simplified LoggerAdaptor and added Ruby code to SK::Rt::Scope to handle output for differenct log levels.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@512 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- extensions/sk/LoggerAdaptor.h
+++ extensions/sk/LoggerAdaptor.h
@@ -16,43 +16,33 @@ namespace ruby {
   {
     public:
       LoggerAdaptor(const sk::rt::Scope& scope)
-        : _label(sk::util::String::EMPTY), _scope(scope) {}
-
-      LoggerAdaptor(const sk::util::String& label, const sk::rt::Scope& scope)
-        : _labelStore(label), _label(_labelStore), _scope(scope) {}
-
-      LoggerAdaptor(const LoggerAdaptor& other)
-        : _labelStore(other._labelStore), 
-          _label(&other._label == &sk::util::String::EMPTY ? other._label : _labelStore), 
-          _scope(other._scope) {}
+        : _scope(scope) {}
 
       bool error(const sk::util::String& message) const {
-        return (_scope.error(_label) << message).isEnabled();
+        return (_scope.error() << message).isEnabled();
       }
       
       bool warning(const sk::util::String& message) const {
-        return (_scope.warning(_label) << message).isEnabled();
+        return (_scope.warning() << message).isEnabled();
       }
 
       bool notice(const sk::util::String& message) const {
-        return (_scope.notice(_label) << message).isEnabled();
+        return (_scope.notice() << message).isEnabled();
       }
 
       bool info(const sk::util::String& message) const {
-        return (_scope.info(_label) << message).isEnabled();
+        return (_scope.info() << message).isEnabled();
       }
 
       bool debug(const sk::util::String& message) const {
-        return (_scope.debug(_label) << message).isEnabled();
+        return (_scope.debug() << message).isEnabled();
       }
 
       bool detail(const sk::util::String& message) const {
-        return (_scope.detail(_label) << message).isEnabled();
+        return (_scope.detail() << message).isEnabled();
       }
 
     private:
-      const sk::util::String _labelStore;
-      const sk::util::String& _label;
       const sk::rt::Scope& _scope;
   };
 }
