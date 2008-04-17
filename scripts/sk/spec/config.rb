380e9492fc1cfe19a3c863ed931dcca2066fedf0
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-04-17 07:22:22 +0000

    Added config setup for running ruby specs.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@637 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ scripts/sk/spec/config.rb
@@ -0,0 +1,28 @@
+=begin
+  Copyright (c) 2008, Gennady Bystritsky <bystr@mac.com>
+  
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+  
+  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+=end
+
+require 'spec'
+
+Spec::Runner.configure do |config|
+  # == Mock Framework
+  #
+  # RSpec uses it's own mocking framework by default. If you prefer to
+  # use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  config.mock_with :mocha
+
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+end
+
+begin
+  require 'spec_helper'
+rescue LoadError
+end
