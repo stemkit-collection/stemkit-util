c5f969dc12a4f7146f5d066878e2191113429918
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-09-08 18:27:09 +0000

    Added synchonization tests for data population.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@692 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/sync-master.rb
+++ scripts/sk/sync-master.rb
@@ -56,18 +56,59 @@ if $0 == __FILE__ or defined?(Test::Unit::TestCase)
   require 'mocha'
   require 'stubba'
 
+  require 'sk/executor.rb'
+  require 'enumerator'
+
   module SK
     class SyncMasterTest < Test::Unit::TestCase
-      attr_reader :lock
+      attr_reader :lock, :executor, :depot
 
-      def test_synchronize
-        lock.synchronize do
-          assert true
+      def test_populate_with_lock_must_succeed
+        assert make_slices(1000, 500).all? { |_item|
+          _item == 1
+        }
+      end
+
+      def test_populate_no_lock_must_fail
+        lock.expects(:synchronize).at_least_once.yields
+        assert make_slices(10, 5).any? { |_item|
+          _item != 1
+        }
+      end
+
+      def make_slices(runs, amount)
+        populate(runs, amount, 1)
+        populate(runs, amount, 2)
+        populate(runs, amount, 3)
+
+        executor.join
+        assert_equal runs * amount * 3, depot.size
+
+        slices = depot.enum_slice(amount).map
+        assert_equal runs * 3, slices.size
+
+        slices.map { |_slice|
+          _slice.uniq.size
+        }
+      end
+
+      def populate(runs, amount, data)
+        executor.in_a_thread do
+          runs.times do
+            lock.synchronize do
+              amount.times do 
+                Thread.pass
+                depot.push data
+              end
+            end
+          end
         end
       end
 
       def setup
+        @depot = []
         @lock = SyncMaster.new
+        @executor = SK::Executor.new
       end
     end
   end
