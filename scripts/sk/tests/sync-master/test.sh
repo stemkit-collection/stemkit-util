a3688be1a568804c550943cca52b5fc0b220c24b
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-12-11 00:33:12 +0000

    For Windows - use this script instead of 'make jtest'.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@817 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ scripts/sk/tests/sync-master/test.sh
@@ -0,0 +1,15 @@
+
+jruby.bat -rjava -rrubygems -I"$PATH"  anounce-fail-not-owner-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  capacity-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  ensure-fail-not-owner-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  ensure-success-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  ensure-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  ensure-timeout-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  java-error-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  many-threads-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  non-blocking-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  populate-with-lock-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  ruby-error-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  wakeall-test.rb
+jruby.bat -rjava -rrubygems -I"$PATH"  wakeone-test.rb
+
