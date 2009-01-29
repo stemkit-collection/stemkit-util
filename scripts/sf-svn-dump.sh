308e3afe51d252eb5b578dbe6e4cc77087034e32
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-01-29 18:40:38 +0000

    Cosmetic changes.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@899 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sf-svn-dump.sh
+++ scripts/sf-svn-dump.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# vim: set sw=2:
 
 [ "${#}" -ne 1 ] && {
   echo "USAGE: ${0} <repository>" 1>&2
@@ -6,11 +8,12 @@
 
 repository="${1}"
 echo "Repository: ${repository}"
-sleep 3
-
 rm -rf "${repository}"
-rsync -avq "${repository}".svn.sourceforge.net::svn/"${repository}"/\* "${repository}"
-head_revision=`svnlook youngest "${repository}"`
-
-svnadmin dump -q --deltas -r 0:"${head_revision}" "${repository}" | bzip2 -c > "${repository}.0-${head_revision}.svn-dump.bz2"
-rm -rf "${repository}"
+rsync -avq "${repository}".svn.sourceforge.net::svn/"${repository}"/\* "${repository}" && {
+  head_revision=`svnlook youngest "${repository}"`
+  [ "${head_revision:+set}" = set ] && {
+    echo "Head revision: ${head_revision}"
+    svnadmin dump -q --deltas -r 0:"${head_revision}" "${repository}" | bzip2 -c > "${repository}.0-${head_revision}.svn-dump.bz2"
+    rm -rf "${repository}"
+  }
+}
