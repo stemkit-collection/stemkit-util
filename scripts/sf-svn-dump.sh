29df7b58d33c169328e7a001ae4b653c26dc9e2f
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-04-24 03:37:35 +0000

    Fixing remote repository dumps.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1215 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sf-svn-dump.sh
+++ scripts/sf-svn-dump.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 # vim: set sw=2:
 
-[ "${#}" -ne 1 ] && {
-  echo "USAGE: ${0} <repository>" 1>&2
+[ "${#}" -eq 0 ] && {
+  echo "USAGE: ${0} <repository> ..." 1>&2
   exit 2
 }
 
-repository="${1}"
-echo "Repository: ${repository}"
-rm -rf "${repository}"
-rsync -avq "${repository}".svn.sourceforge.net::svn/"${repository}"/\* "${repository}" && {
-  head_revision=`svnlook youngest "${repository}"`
-  [ "${head_revision:+set}" = set ] && {
-    echo "Head revision: ${head_revision}"
-    svnadmin dump -q --deltas -r 0:"${head_revision}" "${repository}" | bzip2 -c > "${repository}.0-${head_revision}.svn-dump.bz2"
-    rm -rf "${repository}"
+while [ ${#} -ne 0 ]; do 
+  repository="${1}"
+  echo "Repository: ${repository}"
+  rm -rf "${repository}"
+  rsync -avq "${repository}".svn.sourceforge.net::svn/"${repository}"/\* "${repository}" && {
+    head_revision=`svnlook youngest "${repository}"`
+    [ "${head_revision:+set}" = set ] && {
+      echo "Head revision: ${head_revision}"
+      svnadmin dump -q --deltas -r 0:"${head_revision}" "${repository}" | bzip2 -c > "${repository}.0-${head_revision}.svn-dump.bz2"
+      rm -rf "${repository}"
+    }
   }
-}
+  shift
+done
