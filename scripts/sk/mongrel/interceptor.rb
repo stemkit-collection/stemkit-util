e84a532d3e271b8872d29df41fc0f4f629d7746a
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-06-16 14:26:44 +0000

    Implemented mongrel interception.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1349 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/mongrel/interceptor.rb
+++ scripts/sk/mongrel/interceptor.rb
@@ -12,6 +12,42 @@
 module SK
   module Mongrel
     class Interceptor
+      ENVIRONMENT_MARKER = 'sk.mongrel.interceptor'
+      class << self
+        def intercept_unless_already(top)
+          return unless defined? Mongrel::Command::Base
+          return if ENV[ENVIRONMENT_MARKER] == "active"
+
+          ObjectSpace.each_object(Mongrel::Command::Base) do |_object|
+            if _object.class.name.split("::").last == "Start"
+              ENV[ENVIRONMENT_MARKER] = "active"
+              self.new(top).start(_object.original_args)
+            end
+          end
+        end
+      end
+
+      def initialize(top)
+        @top = top
+      end
+
+      def start(*args)
+        close_mongrel_listeners
+        exec command, 'start', *args.flatten
+      end
+
+      private
+      #######
+
+      def command
+        File.join(@top, 'script', 'mongrel_interceptor')
+      end
+
+      def close_mongrel_listeners
+        ObjectSpace.each_object(TCPSocket) do |_object|
+          _object.close
+        end
+      end
     end
   end
 end
