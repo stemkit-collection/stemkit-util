ae1832322512febfac06182467e426cb2de537df
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-01-29 19:36:46 +0000

    Added classes for managing svn repositories, revisions and depos.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2098 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ scripts/sk/svn/depot.rb
@@ -0,0 +1,32 @@
+=begin
+  vim: sw=2:
+  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+  
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+  
+  Author: Gennady Bystritsky <bystr@mac.com>
+=end
+
+module SK
+  module Svn
+    class Depot
+    end
+  end
+end
+
+if $0 == __FILE__ or defined?(Test::Unit::TestCase)
+  require 'test/unit'
+  require 'mocha'
+  require 'stubba'
+
+  module SK
+    module Svn
+      class DepotTest < Test::Unit::TestCase
+        def setup
+        end
+      end
+    end
+  end
+end
