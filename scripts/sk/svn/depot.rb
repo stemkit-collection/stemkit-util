de2576bcfa170d72ce120fea115c9358c0ad21ab
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-02-03 06:47:37 +0000

    Added listener invocation on command start and finish.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2107 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/svn/depot.rb
+++ scripts/sk/svn/depot.rb
@@ -13,17 +13,21 @@ require 'pathname'
 require 'sk/svn/repository.rb'
 require 'tsc/dataset.rb'
 require 'tsc/launch.rb'
+require 'sk/ruby.rb'
 
 module SK
   module Svn
     class Depot
+      include SK::Ruby
       attr_reader :repositories
 
-      DEFAULT_PARAMS = { :location => "~/depots" }
+      DEFAULT_PARAMS = { :location => "~/depots", :listener => nil }
 
       def initialize(params = {})
         @params = TSC::Dataset.new(DEFAULT_PARAMS).update(normalize_params(params))
         @launcher = TSC::Launcher.new
+
+        @params.listener ||= self
 
         @repositories = find_repository_folders(@params.location).map { |_folder|
           Repository.new self, :name => _folder.basename.to_s, :path => _folder
@@ -37,10 +41,23 @@ module SK
       end
 
       def launch(*args)
-        begin
-          @launcher.launch(normalize_command_args(args)).first
-        rescue TSC::Launcher::TerminateError => error
-          raise error.errors.first
+        with TSC::Launcher.normalize_command_args(args) do |_args|
+          begin
+            TSC::Error.ignore {
+              @params.listener.svn_command_started(_args)
+            }
+            with @launcher.launch(_args).first do |_result|
+              TSC::Error.ignore {
+                @params.listener.svn_command_finished(_args, 0)
+              }
+              _result
+            end
+          rescue TSC::Launcher::TerminateError => error
+            TSC::Error.ignore {
+              @params.listener.svn_command_finished(_args, error.exited? ? error.status : -(error.signal))
+            }
+            raise error.errors.first
+          end
         end
       end
       
@@ -49,12 +66,6 @@ module SK
 
       def normalize_params(params)
         Hash === params ? params : Hash[ :location => params ]
-      end
-
-      def normalize_command_args(*args)
-        args.flatten.compact.map { |_item|
-          _item.to_s
-        }
       end
 
       def find_repository_folders(location)
@@ -75,7 +86,19 @@ if $0 == __FILE__ or defined?(Test::Unit::TestCase)
   require 'test/unit'
   require 'mocha'
 
-  depot = SK::Svn::Depot.new '~svn/depots'
+  class Listener
+    class << self
+      def svn_command_started(args)
+        puts ">>> STARTED: #{TSC::Launcher.normalize_command_line(args)}"
+      end
+
+      def svn_command_finished(args, status)
+        puts ">>> FINISHED:#{status}: #{TSC::Launcher.normalize_command_line(args)}"
+      end
+    end
+  end 
+
+  depot = SK::Svn::Depot.new :location => '~svn/depots', :listener => Listener
   spo = depot.repository('spo')
 
   p spo.local_url
