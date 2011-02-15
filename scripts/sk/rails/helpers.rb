bbeb0b9a64534ccac30fce791f95fb1ca9ff99e5
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-02-15 17:23:14 +0000

    Added helpers for flash rendering and update.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2116 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ scripts/sk/rails/helpers.rb
@@ -0,0 +1,32 @@
+=begin
+  vim: sw=2:
+  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+  
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+  
+  Author: Gennady Bystritsky
+=end
+
+module SK
+  module Rails
+    def render_flash_for(area, &block)
+    end
+  end
+end
+
+if $0 == __FILE__ or defined?(Test::Unit::TestCase)
+  require 'test/unit'
+  require 'mocha'
+  require 'stubba'
+
+  module SK
+    module Rails
+      class HelpersTest < Test::Unit::TestCase
+        def setup
+        end
+      end
+    end
+  end
+end
---
+++ scripts/sk/rails/js/helpers.rb
@@ -0,0 +1,36 @@
+=begin
+  vim: sw=2:
+  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+  
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+  
+  Author: Gennady Bystritsky
+=end
+
+module SK
+  module Rails
+    module JS
+      def update_flash_for(area, &block)
+      end
+    end
+  end
+end
+
+if $0 == __FILE__ or defined?(Test::Unit::TestCase)
+  require 'test/unit'
+  require 'mocha'
+  require 'stubba'
+
+  module SK
+    module Rails
+      module JS
+        class HelpersTest < Test::Unit::TestCase
+          def setup
+          end
+        end
+      end
+    end
+  end
+end
