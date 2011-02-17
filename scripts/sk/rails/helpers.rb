62cf7130f253d09717783e05ec620b6053742367
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-02-17 20:16:35 +0000

    Added ability to specify several areas for rendering same partial.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2129 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/rails/helpers.rb
+++ scripts/sk/rails/helpers.rb
@@ -13,18 +13,20 @@ module SK
   module Rails
     module Helpers
       def sk_render_for_area(area, options = {}, &block)
-        area_tag_class = "#{area}-area"
-        block ||= proc { |_area, _class, _content|
-          content_tag :div, _content.to_s, :class => _class
-        }
-        renderer = proc { |_partial|
-          render(:partial => _partial.to_s, :object => area.to_sym, :locals => options).to_s.tap { |_content|
-            break if _content.strip.empty?
+        raw Array(area).map { |_area|
+          area_tag_class = "#{_area}-area"
+          block ||= proc { |_area, _class, _content|
+            content_tag :div, _content.to_s, :class => _class
           }
-        }
-        block.call area, area_tag_class, options[:partial].tap { |_partial|
-          break renderer.call _partial unless _partial.nil?
-        }
+          renderer = proc { |_partial|
+            render(:partial => _partial.to_s, :object => _area.to_sym, :locals => options).to_s.tap { |_content|
+              break if _content.strip.empty?
+            }
+          }
+          block.call _area, area_tag_class, options[:partial].tap { |_partial|
+            break renderer.call _partial unless _partial.nil?
+          }
+        }.compact.join("\n")
       end
     end
   end
