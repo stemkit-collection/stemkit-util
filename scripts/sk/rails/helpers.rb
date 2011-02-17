bbd18f5e50d5342fdc767144885ee0ad78d5ff1e
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-02-17 22:48:00 +0000

    Refactored partial determination as a preparation for the introduction of shared partials.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2131 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/rails/helpers.rb
+++ scripts/sk/rails/helpers.rb
@@ -12,19 +12,24 @@
 module SK
   module Rails
     module Helpers
+      class << self
+        def with_options_for_partial_or_nil(options, &block)
+          options[:partial].tap { |_partial|
+            break block.call _partial, options unless _partial.nil?
+          }
+        end
+      end
+
       def sk_render_for_area(area, options = {}, &block)
         raw Array(area).map { |_area|
           area_tag_class = "#{_area}-area"
           block ||= proc { |_area, _class, _content|
             content_tag :div, _content.to_s, :class => _class
           }
-          renderer = proc { |_partial|
-            render(:partial => _partial.to_s, :object => _area.to_sym, :locals => options).to_s.tap { |_content|
+          block.call _area, area_tag_class, SK::Rails::Helpers.with_options_for_partial_or_nil(options) { |_partial, _options|
+            render(:partial => _partial.to_s, :object => _area.to_sym, :locals => _options).to_s.tap { |_content|
               break if _content.strip.empty?
             }
-          }
-          block.call _area, area_tag_class, options[:partial].tap { |_partial|
-            break renderer.call _partial unless _partial.nil?
           }
         }.compact.join("\n")
       end
@@ -43,6 +48,10 @@ if $0 == __FILE__ or defined?(Test::Unit::TestCase)
 
         def render(*args)
           args.inspect
+        end
+
+        def raw(content)
+          content
         end
 
         def content_tag(name, content_or_options = {}, options = {}, &block)
