a241fb0e28ca2186fb5be0b4430b7d2c0bd9f2f9
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-10-25 07:11:54 +0000

    Enhanced SK::Net::Entpoint class to allow creation with another endpoint (like C++ copy constructor), as well as with just a port. In the latter case the host name is set to "localhost".
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2562 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/net/endpoint.rb
+++ scripts/sk/net/endpoint.rb
@@ -14,11 +14,9 @@ require 'tsc/errors.rb'
 module SK
   module Net
     class Endpoint
-      attr_reader :host, :port
-
       class NoHostError < TSC::Error
-        def initialize
-          super "Host not specified"
+        def initialize(spec)
+          super "Host not specified in #{spec.inspect}"
         end
       end
 
@@ -28,15 +26,31 @@ module SK
         end
       end
 
+      attr_reader :host, :port
+
       def initialize(*args)
         host, *default_ports = args.flatten.compact.tap { |_hostspec, *_ports|
-          break [ _hostspec[:host], _hostspec[:port], *_ports ] if Hash === _hostspec
+          case _hostspec
+            when String 
+              next
+
+            when Hash 
+              break [ _hostspec[:host], _hostspec[:port], *_ports ]
+
+            when self.class
+              break [ _hostspec.host, _hostspec.port, *_ports ]
+
+            when Fixnum
+              break [ "localhost", _hostspec, *_ports ]
+
+            else raise NoHostError, _hostspec
+          end
         }
         host.to_s.split(':').tap { |_host, *_ports|
           @host = _host.to_s.strip
           @port = filter_ports(_ports.reverse + default_ports).flatten.compact.first.to_i
         }
-        raise NoHostError if @host.empty?
+        raise NoHostError, host if @host.empty?
         raise NoPortError, @host if @port.zero?
       end
 
@@ -79,38 +93,64 @@ if $0 == __FILE__
           end
         end
 
+        def test_just_port
+          endpoint = SK::Net::Endpoint.new 245
+
+          assert_equal "localhost", endpoint.host
+          assert_equal 245, endpoint.port
+        end
+
         def test_simple_host_and_numeric_port
           endpoint = SK::Net::Endpoint.new "aaa", 45
+
           assert_equal "aaa", endpoint.host
           assert_equal 45, endpoint.port
-
           assert_equal "aaa:45", endpoint.to_s
         end
 
         def test_simple_host_and_several_ports
           endpoint = SK::Net::Endpoint.new "aaa", "hohoho", "17", 45
+
           assert_equal "aaa", endpoint.host
           assert_equal 17, endpoint.port
-
           assert_equal '"aaa:17"', endpoint.inspect
         end
 
         def test_simple_host_with_port
           endpoint = SK::Net::Endpoint.new "aaa:55", "hohoho", "17", 45
+
           assert_equal "aaa", endpoint.host
           assert_equal 55, endpoint.port
         end
 
         def test_simple_host_with_several_ports
           endpoint = SK::Net::Endpoint.new "bbb:55:1", "hohoho", "17", 45
+
           assert_equal "bbb", endpoint.host
           assert_equal 1, endpoint.port
         end
 
         def test_hashed_host_and_port
           endpoint = SK::Net::Endpoint.new :host => "uuu", :port => 19
+
           assert_equal "uuu", endpoint.host
           assert_equal 19, endpoint.port
+        end
+
+        def test_other_endpoint
+          other_endpoint = SK::Net::Endpoint.new :host => "zzz", :port => 21
+          endpoint = SK::Net::Endpoint.new other_endpoint
+
+          assert_equal "zzz", endpoint.host
+          assert_equal 21, endpoint.port
+        end
+
+        def test_other_endpoint_with_port
+          other_endpoint = SK::Net::Endpoint.new :host => "zzz", :port => 21
+          endpoint = SK::Net::Endpoint.new other_endpoint, 123
+
+          assert_equal "zzz", endpoint.host
+          assert_equal 21, endpoint.port
         end
 
         def setup
