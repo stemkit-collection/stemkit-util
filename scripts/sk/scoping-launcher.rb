5c53de6f97f95f648f79a4e919bccd97136e6ccd
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-12-18 21:04:24 +0000

    Added unit tests for different scenarious.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2616 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/scoping-launcher.rb
+++ scripts/sk/scoping-launcher.rb
@@ -25,7 +25,15 @@ module SK
     end
 
     def reason(selectors)
-      [ 'no', (selectors.join(', ') unless selectors.empty?) ].compact.join(' ')
+      [ 'no', selector_label(selectors), selector_list(selectors) ].compact.join(' ')
+    end
+
+    def selector_label(selectors)
+      "selector#{'s' unless selectors.size == 1}"
+    end
+
+    def selector_list(selectors)
+      selectors.join(', ') unless selectors.empty?
     end
   end
 
@@ -117,6 +125,50 @@ if $0 == __FILE__
 
   module SK
     class ScopingLauncherTest < Test::Unit::TestCase
+      def test_exits_if_not_subclassed
+        SK::ScopingLauncher.any_instance.expects(:print_error).with instance_of(TSC::NotImplementedError)
+        assert_raises SystemExit do
+          SK::ScopingLauncher.new.start
+        end
+      end
+
+      def test_fails_if_no_scope_selectors
+        SK::ScopingLauncher.new.tap { |_app|
+          error = assert_raises SK::SelectableScopeError do
+            _app.local_scope_top
+          end
+
+          assert_equal 'Local scope top not found (no selectors)', error.message
+        }
+      end
+
+      def test_fails_if_single_scope_selector_not_found
+        SK::ScopingLauncher.any_instance.expects(:local_scope_selectors).returns 'aaa/bbb/ccc'
+
+        SK::ScopingLauncher.new.tap { |_app|
+          error = assert_raises SK::SelectableScopeError do
+            _app.local_scope_top
+          end
+
+          assert_equal 'Local scope top not found (no selector aaa/bbb/ccc)', error.message
+        }
+      end
+
+      def test_fails_if_multiple_scope_selectors_not_found
+        SK::ScopingLauncher.any_instance.expects(:local_scope_selectors).returns [ 
+          'aaa/bbb/ccc',
+          'zzz/uuu/bbb'
+        ]
+
+        SK::ScopingLauncher.new.tap { |_app|
+          error = assert_raises SK::SelectableScopeError do
+            _app.local_scope_top
+          end
+
+          assert_equal 'Local scope top not found (no selectors aaa/bbb/ccc, zzz/uuu/bbb)', error.message
+        }
+      end
+
       def setup
       end
     end
