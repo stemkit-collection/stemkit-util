4ae381ea10889837925d3e0e9a07d8bbc4928ce2
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-12-26 08:58:49 +0000

    Added a class to collect all the values in config files.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2656 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ scripts/sk/config/collector.rb
@@ -0,0 +1,91 @@
+=begin
+  vim: sw=2:
+  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+
+  Author: Gennady Bystritsky
+=end
+
+require 'tsc/dataset.rb'
+
+require 'sk/config/data.rb'
+require 'sk/config/data-interpolator.rb'
+require 'sk/config/uproot-locator.rb'
+require 'sk/config/home-locator.rb'
+
+module SK
+  module Config
+    class Collector
+      DEFAULTS = {
+        :uproot => false,
+        :home => false,
+        :spot => '.'
+      }
+      def initialize(attributor = nil)
+        @attributor = attributor
+      end
+
+      def collect(name, options = {})
+        TSC::Dataset.new(DEFAULTS).update(options).tap { |_options|
+          locator(name, _options).invoke(self)
+          break config
+        }
+      end
+
+      def process(content, location)
+        registry[location] ||= begin
+          Hash[ (YAML.parse(content) || self).transform ].tap { |_hash|
+            config.update SK::Config::DataInterpolator.new(location, @attributor).transform(_hash)
+          }
+        end
+      end
+
+      def config
+        @config ||= SK::Config::Data.new
+      end
+
+      def transform
+        Hash.new
+      end
+
+      private
+      #######
+
+      def registry
+        @registry ||= {}
+      end
+
+      def locator(name, options)
+        (options.uproot ? SK::Config::UprootLocator : SK::Config::SpotLocator).tap { |_class|
+          return _class.new :item => name, :spot => options.spot, :locator => home_locator(name, options)
+        }
+      end
+
+      def home_locator(name, options)
+        SK::Config::HomeLocator.new(:item => String === options.home ? options.home : name) if options.home
+      end
+    end
+  end
+end
+
+if $0 == __FILE__
+  require 'test/unit'
+  require 'mocha'
+
+  p SK::Config::Collector.new(nil).collect('.buildrc', :uproot => true, :home => true).to_hash
+
+  module SK
+    module Config
+      class CollectorTest < Test::Unit::TestCase
+        def test_nothing
+        end
+
+        def setup
+        end
+      end
+    end
+  end
+end
