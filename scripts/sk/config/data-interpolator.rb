547ce8193a9123e93671040eb60b0b63eb4b1a78
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-12-27 02:50:14 +0000

    Using %{} instead of #{} for interpolating config values not to step on YAML's processing of #{} and for general consistency.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2663 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/config/data-interpolator.rb
+++ scripts/sk/config/data-interpolator.rb
@@ -21,7 +21,7 @@ module SK
       def transform(data)
         case data
           when String
-            data.gsub(%r{(%[Rr])|(?:[#][{]\s*(\w+)\s*[}])}) { |_match|
+            data.gsub(%r{(%[Rr])|(?:[%][{]\s*(\w+)\s*[}])}) { |_match|
               next @location if $1
               next @attributes.send($2) if @attributes and $2
 
@@ -59,29 +59,29 @@ if $0 == __FILE__
           assert_equal 'hohoho', interpolator.transform('hohoho')
           assert_equal '/aaa/bbb/ccc', interpolator.transform('%r')
           assert_equal '--/aaa/bbb/ccc--/aaa/bbb/ccc', interpolator.transform('--%R--%r')
-          assert_equal '--/aaa/bbb/ccc--#{uuu}--', interpolator.transform('--%R--#{uuu}--')
+          assert_equal '--/aaa/bbb/ccc--%{uuu}--', interpolator.transform('--%R--%{uuu}--')
         end
 
         def test_attributes_transformed_in_strings
           attributes = TSC::Dataset.new :fff => 'v1', :abc => 'v2'
           interpolator = SK::Config::DataInterpolator.new '/aaa/bbb/ccc', attributes
 
-          assert_equal 'v1', interpolator.transform('#{fff}')
-          assert_equal '--v1--v2--v1--', interpolator.transform('--#{fff}--#{abc}--#{fff}--')
+          assert_equal 'v1', interpolator.transform('%{fff}')
+          assert_equal '--v1--v2--v1--', interpolator.transform('--%{fff}--%{abc}--%{fff}--')
         end
 
         def test_arrays_transformed
           attributes = TSC::Dataset.new :fff => 'v1', :abc => 'v2'
           interpolator = SK::Config::DataInterpolator.new '/a/b/c', attributes
 
-          assert_equal [ [ '/a/b/c', 'v1' ], '/a/b/c--v1--v2' ], interpolator.transform([ [ '%r', '#{fff}' ], '%r--#{fff}--#{abc}' ])
+          assert_equal [ [ '/a/b/c', 'v1' ], '/a/b/c--v1--v2' ], interpolator.transform([ [ '%r', '%{fff}' ], '%r--%{fff}--%{abc}' ])
         end
 
         def test_hashes_transformed
           attributes = TSC::Dataset.new :fff => 'v1', :abc => 'v2'
           interpolator = SK::Config::DataInterpolator.new '/a/b/c', attributes
 
-          assert_equal Hash[ :aaa => [ '/a/b/c', 'v1' ], :bbb => '/a/b/c--v1--v2' ], interpolator.transform( :aaa => [ '%r', '#{fff}' ], :bbb => '%r--#{fff}--#{abc}' )
+          assert_equal Hash[ :aaa => [ '/a/b/c', 'v1' ], :bbb => '/a/b/c--v1--v2' ], interpolator.transform( :aaa => [ '%r', '%{fff}' ], :bbb => '%r--%{fff}--%{abc}' )
         end
 
         def test_other_types_stay_intact
