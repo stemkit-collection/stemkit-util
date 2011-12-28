ae3bddf89357c570ac74b5e7b8c48a7b462197e3
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-12-28 08:44:29 +0000

    Implemented a class to track file statuses and provide appropriate serialization.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2680 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ scripts/sk/cvs/cli/file.rb
@@ -0,0 +1,173 @@
+=begin
+  vim: sw=2:
+  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+
+  Author: EMC Gennady Bystritsky
+=end
+
+require 'pathname'
+
+module SK
+  module Cvs
+    module Cli
+      class File
+        attr_reader :path, :status
+
+        attr_accessor :working_revision, :repository_revision, :repository_path
+        attr_accessor :commit_identifier, :sticky_tag, :sticky_date, :sticky_options
+
+        def initialize(name, folder, status)
+          @status = self.class.statuses.fetch(status, '<' + status.to_s + '>')
+          @path = folder.join(missing? ? name.sub(%r{^no file\s*}, '') : name)
+        end
+
+        def to_s
+          join_items ' ', [
+            status,
+            ' ' * 2,
+            path,
+            outdated? ? [ '...', working_revision, '->', repository_revision ] : []
+          ]
+        end
+
+        def updated?
+          modified? || added? || local_only? || missing?
+        end
+
+        def current?
+          status == 'U'
+        end
+
+        def missing?
+          status == '!'
+        end
+
+        def modified?
+          [ 'M', 'G' ].include? status
+        end
+
+        def outdated?
+          [ '*', 'G' ].include? status
+        end
+
+        def local_only?
+          status == '?'
+        end
+
+        def added?
+          status == 'A'
+        end
+
+        private
+        #######
+
+        def join_items(separator, *args)
+          args.flatten.compact.join separator
+        end
+
+        class << self
+          def statuses
+            @statuses ||= {
+              'Up-to-date' => 'U',
+              'Locally Added' => 'A',
+              'Locally Modified' => 'M',
+              'Needs Patch' => '*',
+              'Needs Merge' => 'G',
+              'Needs Checkout' => '!',
+              '?' => '?'
+            }
+          end
+        end
+      end
+    end
+  end
+end
+
+if $0 == __FILE__
+  require 'test/unit'
+  require 'mocha'
+
+  module SK
+    module Cvs
+      module Cli
+        class FileTest < Test::Unit::TestCase
+          def test_current
+            Cli::File.new('fff', Pathname.new('d1').join('d2'), 'Up-to-date').tap { |_file|
+              assert_equal true, _file.current?
+              assert_equal false, _file.updated?
+              assert_equal false, _file.outdated?
+              assert_equal 'U    d1/d2/fff', _file.to_s
+            }
+          end
+
+          def test_locally_added
+            Cli::File.new('fff', Pathname.new('d1').join('d2'), 'Locally Added').tap { |_file|
+              assert_equal true, _file.added?
+              assert_equal true, _file.updated?
+              assert_equal false, _file.outdated?
+              assert_equal 'A    d1/d2/fff', _file.to_s
+            }
+          end
+
+          def test_needs_patch
+            Cli::File.new('fff', Pathname.new('d1').join('d2'), 'Needs Patch').tap { |_file|
+              _file.working_revision = '1.1'
+              _file.repository_revision = '2.3'
+
+              assert_equal false, _file.modified?
+              assert_equal false, _file.updated?
+              assert_equal true, _file.outdated?
+              assert_equal '*    d1/d2/fff ... 1.1 -> 2.3', _file.to_s
+            }
+          end
+
+          def test_needs_merge
+            Cli::File.new('fff', Pathname.new('d1').join('d2'), 'Needs Merge').tap { |_file|
+              _file.working_revision = '1.1'
+              _file.repository_revision = '2.3'
+
+              assert_equal true, _file.modified?
+              assert_equal true, _file.updated?
+              assert_equal true, _file.outdated?
+              assert_equal 'G    d1/d2/fff ... 1.1 -> 2.3', _file.to_s
+            }
+          end
+
+          def test_locally_modified
+            Cli::File.new('fff', Pathname.new('d1').join('d2'), 'Locally Modified').tap { |_file|
+              assert_equal true, _file.modified?
+              assert_equal true, _file.updated?
+              assert_equal false, _file.outdated?
+              assert_equal 'M    d1/d2/fff', _file.to_s
+            }
+          end
+
+          def test_missing
+            Cli::File.new('no file fff', Pathname.new('d1').join('d2'), 'Needs Checkout').tap { |_file|
+              assert_equal false, _file.modified?
+              assert_equal true, _file.updated?
+              assert_equal false, _file.outdated?
+              assert_equal '!    d1/d2/fff', _file.to_s
+            }
+          end
+
+          def test_local_only
+            Cli::File.new('aaa/bbb/fff', Pathname.new('.'), '?').tap { |_file|
+              assert_equal true, _file.local_only?
+              assert_equal true, _file.updated?
+              assert_equal false, _file.outdated?
+              assert_equal '?    aaa/bbb/fff', _file.to_s
+            }
+          end
+
+          def setup
+          end
+        end
+      end
+    end
+  end
+end
