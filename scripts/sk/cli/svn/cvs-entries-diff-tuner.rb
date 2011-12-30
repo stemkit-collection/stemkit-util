179363bf26fad5cf466483d067607d9a9e3ff1e2
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-12-30 06:22:52 +0000

    Added tuners for filtering cvs metadata checked-in to svn.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2694 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ scripts/sk/cli/svn/cvs-entries-diff-tuner.rb
@@ -0,0 +1,54 @@
+=begin
+  vim: sw=2:
+  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+
+  Author: Gennady Bystritsky
+=end
+
+require 'sk/cli/tuner.rb'
+
+module SK
+  module Cli
+    module Svn
+      class CvsEntriesDiffTuner < SK::Cli::Tuner
+        def check_option(option)
+          return option unless [ '--no-ignore', '-I' ].include?(option)
+          @noignore = true
+          nil
+        end
+
+        def process(io)
+          io.each do |_line|
+            unless @noignore
+              @noshow = ($2 ? true : false) if _line =~ %r{^Index:\s+(.*?)(CVS/Entries)?\s*$}
+            end
+
+            app.output_info _line unless @noshow
+          end
+
+          display_errors_if_any
+        end
+      end
+    end
+  end
+end
+
+if $0 == __FILE__
+  require 'test/unit'
+  require 'mocha'
+
+  module SK
+    module Cli
+      module Svn
+        class CvsEntriesDiffTunerTest < Test::Unit::TestCase
+          def setup
+          end
+        end
+      end
+    end
+  end
+end
---
+++ scripts/sk/cli/svn/cvs-entries-status-tuner.rb
@@ -0,0 +1,49 @@
+=begin
+  vim: sw=2:
+  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+
+  Author: Gennady Bystritsky
+=end
+
+require 'sk/cli/tuner.rb'
+
+module SK
+  module Cli
+    module Svn
+      class CvsEntriesStatusTuner < SK::Cli::Tuner
+        def check_option(option)
+          @noignore = true if option == '--no-ignore'
+          option
+        end
+
+        def process(io)
+          io.each do |_line|
+            app.output_info _line unless !@noignore and _line =~ %r{CVS/Entries$}
+          end
+
+          display_errors_if_any
+        end
+      end
+    end
+  end
+end
+
+if $0 == __FILE__
+  require 'test/unit'
+  require 'mocha'
+
+  module SK
+    module Cli
+      module Svn
+        class CvsEntriesStatusTunerTest < Test::Unit::TestCase
+          def setup
+          end
+        end
+      end
+    end
+  end
+end
