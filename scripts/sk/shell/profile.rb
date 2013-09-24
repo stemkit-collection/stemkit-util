a609221302ffed2d3e88c6c31b9129c9c2734af9
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2013-09-24 07:03:46 +0000

    Adjusting login procedure to prefer /opt/*-third-party/... location for ruby lookup and eventual path setup.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@3061 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- scripts/sk/shell/profile.rb
+++ scripts/sk/shell/profile.rb
@@ -246,6 +246,7 @@ module SK
               #{home}/local/platform/#{sysid}/bin
               #{home}/local/#{sysid}/bin
               #{home}/local/bin
+              /opt/*-third-party/platform/#{sysid}/bin
               /opt/*/platform/#{sysid}/bin
               /sbin
               /opt/ansic/bin
